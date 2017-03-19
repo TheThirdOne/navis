0f7b74f6c5b4dc3674699c6da7a0a2e0b2b06b63
Benjamin Landers <benjaminrlanders@gmail.com>
2017-03-18 21:06:09 -0700

    Added basic assets and got them on screen

---
+++ main.js
@@ -0,0 +1,84 @@
+var stage = new createjs.Stage("demoCanvas");
+//Create a Shape DisplayObject.
+var radar = new createjs.Shape();
+radar.graphics.setStrokeStyle(6).beginStroke("#00AA00").arc(0, 0, 100, 0, Math.PI/2, false);
+radar.graphics.setStrokeStyle(2).beginStroke("#00AA00").arc(0, 0, 100, -Math.PI/2, -Math.PI/4, false);
+radar.x = radar.y = 200;
+//stage.addChild(radar);
+
+stage.update();
+
+
+function loadAll(srcs,onload){
+  var out;
+  var i = 0;
+  var func = function(){
+    i++;
+    if(i == srcs.length){
+      onload(out);
+    }
+  }
+  out = srcs.map(src=>{var img = new Image(); img.onload = func; img.src = src; return img;})
+}
+
+
+loadAll(["assets/sea.jpg","assets/ship.png", "assets/compass_back.png", "assets/compass_needle.png","assets/wind.png"],onload);
+var game = {};
+
+
+function onload(arr){
+  // Add sea background
+  var back = new createjs.Shape();
+  back.regX = stage.canvas.width/2; back.regY = stage.canvas.height/2
+  back.x = stage.canvas.width/2;back.y = stage.canvas.height/2;
+  back.graphics.beginBitmapFill(arr[0],'repeat').rect(0,0,stage.canvas.width,stage.canvas.height);
+  stage.addChild(back);
+  
+  // Add Ship
+  var ship = new createjs.Bitmap(arr[1]);
+  ship.regX = 150;
+  ship.regY = 150;
+  ship.x = stage.canvas.width/2;
+  ship.y = stage.canvas.height/2;
+  stage.addChild(ship);
+  
+  var compass = new createjs.Bitmap(arr[2]);
+  compass.regX = 150;
+  compass.regY = 150;
+  compass.scaleX = .5;
+  compass.scaleY = .5;
+  compass.x = stage.canvas.width-100;
+  compass.y = stage.canvas.height/2;
+  stage.addChild(compass);
+  
+  var needle = new createjs.Bitmap(arr[3]);
+  needle.regX = 150;
+  needle.regY = 150;
+  needle.scaleX = .4;
+  needle.scaleY = .4;
+  needle.x = stage.canvas.width-100;
+  needle.y = stage.canvas.height/2;
+  stage.addChild(needle);
+  
+  
+  var wind = new createjs.Bitmap(arr[4]);
+  wind.regX = 150;
+  wind.regY = 150;
+  wind.scaleX = .4;
+  wind.scaleY = .4;
+  wind.x = 100;
+  wind.y = stage.canvas.height/4*3;
+  stage.addChild(wind);
+  stage.update();
+  game.entities = {back,ship,compass,needle,wind};
+  requestAnimationFrame(loop);
+}
+
+
+
+function loop(){
+  game.entities.needle.rotation += .4;
+  
+  stage.update();
+  requestAnimationFrame(loop);
+}
